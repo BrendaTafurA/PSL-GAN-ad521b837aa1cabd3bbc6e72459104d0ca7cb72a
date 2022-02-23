449e04adbd6c6f243b6f80f76ff8045825e056e1
BrendaTafurA <tafurbrenda@gmail.com>
2022-02-23 09:57:34 -0500

    first commit

---
+++ utils/parse_arguments.py
@@ -0,0 +1,38 @@
+# Standard library imports
+import argparse
+import random
+import os
+import sys
+import json
+   
+
+def parse_arguments_generate_dataset():
+    ap = argparse.ArgumentParser(description='Using of media pipe in PSL')
+    
+    ap.add_argument('--inputPath', type=str, default="C:/Users/BRENDA\Documents/SOTA/Experimentation/Data_cLSP_version2/Data/Videos/Segmented_gestures/",
+                    help='relative path of images input.')
+
+    ap.add_argument('-wlf', '--withLineFeature', default=False, action='store_true',
+                    help="use line features")
+
+    ap.add_argument('-lhl', '--leftHandLandmarks', default=False, action='store_true',
+                    help='Get left hand landmarks')
+
+    ap.add_argument('-rhl', '--rightHandLandmarks', default=False, action='store_true',
+                    help='Get right hand landmarks')
+
+    ap.add_argument('-fl', '--faceLandmarks', default=False, action='store_true',
+                    help='Get face landmarks')
+
+    ap.add_argument('-mf', '--minframes', type=int, default=10,
+                    help='Number of frames of each video')
+
+    ap.add_argument('-mi', '--mininstances', type=int, default=10,
+                    help='Number of instances of each class')
+
+    ap.add_argument('-aej', '--addExtraJoint', default=False, action='store_true',
+                    help='Add the joint number 33 (middle point of 12 and 11)')
+
+    args = ap.parse_args()
+
+    return args
